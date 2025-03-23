278caced8d00eea9a948c6728798f5d2561ab362
rustyff12 <rustyff123@gmail.com>
2025-03-23 17:45:43 -0400

    Implement search functionality and style SearchPage.

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -13,6 +13,9 @@ export default function Navbar() {
                     <li className="nav-item">
                         <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
                     </li>
+                    <li className="nav-item">
+                        <a className="nav-link" to="/app/search">Search</a>
+                    </li>
                 </ul>
             </div>
         </nav>
