0c1bcae1c683dc216a6e12abb8beac18662d93de
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2025-03-26 17:37:15 +0100

    app.js update

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,27 +1,26 @@
-/*jshint esversion: 8 */
-import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import MainPage from './components/MainPage/MainPage';
-import LoginPage from './components/LoginPage/LoginPage';
-import RegisterPage from './components/RegisterPage/RegisterPage';
-import SearchPage from './components/SearchPage/SearchPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./components/MainPage/MainPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
+import SearchPage from "./components/SearchPage/SearchPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const navigate = useNavigate();
-  return (
-        <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/search" element={<SearchPage/>} />
-        </Routes>
-        </>
-  );
+	// const navigate = useNavigate();
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<MainPage />} />
+				<Route path="/app" element={<MainPage />} />
+				<Route path="/app/login" element={<LoginPage />} />
+				<Route path="/app/register" element={<RegisterPage />} />
+				<Route path="/app/search" element={<SearchPage />} />
+			</Routes>
+		</>
+	);
 }
 export default App;
